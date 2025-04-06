fad195608eec7eda30b26e155d06d2fb87a9910a
Silvia <mehedihasan2002.myc@gmail.com>
2025-04-06 19:32:04 +0600

    fix: neo-tree toggle

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -16,6 +16,6 @@ return {
 
   config = function()
 
-    vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<C-e>", "<Cmd>Neotree toggle<CR>", {})
   end
 }
